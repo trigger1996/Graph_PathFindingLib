ba7557dec9194ca12f65f57fa5b471ab2a10f3f1
trigger1996 <529388771@qq.com>
2018-08-13 09:47:43 +0800

    removed some unnecessary compiling files

--- BFS_Complete/include/graph_simple_w_bfs.h
+++ BFS_Complete/include/graph_simple_w_bfs.h
@@ -1,6 +1,8 @@
 #ifndef __Graph_Simple_w_BFS_H
 #define __Graph_Simple_w_BFS_H
 
+#include "node.h"
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -9,6 +11,10 @@
 #include <eigen3/Eigen/Dense>
 #include <opencv2/opencv.hpp>
 #include "../../graph_lib/include/graph_simple.h"
+
+using namespace std;
+using namespace cv;
+using namespace Eigen;
 
 class __graph_simple_bfs : public __graph_simple {
 public:
@@ -25,7 +31,22 @@ public:
 
     }
 
+    void run_BFS();
+
+    void draw_Img_with_Route(bool is_show);
+
+    void draw_Img_with_Route(String winName);
+
+    void print_Img_with_Route();
+
 protected:
+
+    Point2f startPos;
+    Point2f endPos;
+
+    vector<__node> bfs_q;           // bfs_quene
+
+    vector<__node> route;
 
 };
 
